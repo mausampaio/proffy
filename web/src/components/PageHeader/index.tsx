6686311e46f0a04763655b082e0e0448751147a7
Mauricio Sampaio <mausampaio@gmail.com>
2020-08-06 17:08:33 -0300

    Teacher Form created

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,9 +8,10 @@ import './styles.css';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => (
+const PageHeader: React.FC<PageHeaderProps> = ({ title, description, children }) => (
   <header className="page-header">
     <div className="top-bar-container">
       <Link to="/">
@@ -21,6 +22,7 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => (
 
     <div className="header-content">
       <strong>{title}</strong>
+      { description && <p>{description}</p> }
 
       {children}
     </div>
---
+++ web/src/components/Textarea/index.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { TextareaHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  name: string;
+  label: string;
+}
+
+const Textarea: React.FC<TextareaProps> = ({ name, label, ...rest }) => (
+  <div className="textarea-block">
+    <label htmlFor={name}>
+      {label}
+      <textarea id={name} {...rest} />
+    </label>
+  </div>
+);
+
+export default Textarea;
